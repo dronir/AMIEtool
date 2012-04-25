f1273e688a372b209d62cd329902661ad2d21fa5
Olli Wilkman <olli.wilkman@iki.fi>
2012-04-25 12:36:19 +0300

    Start with existing versions

---
+++ listImages.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+from sys import exit
+
+def getAreas(filename):
+	"""Get list of areas from file as list of tuples of floats (lon_min, lon_max, lat_min, lat_max)."""
+	areas = []
+	try: 
+		f = file(filename)
+	except IOError:
+		print "Error opening file %s!" % (filename)
+		exit()
+	for line in f:
+		areas.append(tuple(map(float, line.split())))
+	return areas
+
+def checkCoordinates(lon, lat, area):
+	"""Check whether given lon and lat are within limits given in tuple."""
+	if lon >= area[0] and lon <= area[1] and lat >= area[2] and lat <= area[3]:
+		return True
+	else:
+		return False
+
+def checkAreas(lon, lat, areas=None):
+	"""Given list of areas, return True if point is in any of them."""
+	if areas == None:
+		return False
+	else:
+		return reduce(lambda x,y: x or y, [checkCoordinates(lon, lat, area) for area in areas])
+
+def printList(filters=[], areas=None):
+	"""Given list of filter numbers and list of areas, print paths to all AMIE images in those areas.
+	
+	'areas' must be a list of tuples (lon_min, lon_max, lat_min, lat_max).
+	'filters' must be a list containing numbers between 1 and 8, corresponding to the different AMIE filters.
+	
+	Both parameters, 'filters' and 'areas' are optional. 
+	1. If filters is given and areas is not, the code prints all AMIE images from those filters 
+	(anywhere on the surface). 
+	2. If areas is given and filters not, print images from all filters in those regions.
+	3. If neither is given, print list of all AMIE images."""
+
+	ftp_path_base = "ftp://anonymous@psa.esac.esa.int/pub/mirror/SMALL-MISSIONS-FOR-ADVANCED-RESEARCH-AND-TECHNOLOGY/AMIE/S1-L-X-AMIE-3-RDR-%s-V1.0/DATA"
+	filename_base = "GEO_MOON_%s.TAB"
+
+	for phase in ["LP", "EP"]:
+		filename = filename_base % phase
+		ftp_path = ftp_path_base % phase
+		try:
+			f = file(filename)
+		except IOError:
+			print "Error reading file %s!" % (filename)
+			exit()
+		for line in f:
+			line = line.split(",")
+			filter_no = int(line[4])
+			if filters and not filter_no in filters: # No need to go on
+				continue
+			# Get filename, centre latitude and longitude, then
+			# print file path if lat/lon in given areas.
+			filename = line[0].strip('".')
+			cent_lon = float(line[10])
+			cent_lat = float(line[15])
+			timestamp = line[1]
+			orbit = line[2]
+			image = line[3]
+			if checkAreas(cent_lon, cent_lat, areas):
+				print ftp_path + filename
+				
+
+if __name__=="__main__":
+	from sys import argv
+	if len(argv) == 2:
+		filename = argv[1]
+		filters = []
+	elif len(argv) == 3 and argv[1].startswith("-f"):
+		filename = argv[2]
+		filters = map(int, list(argv[1][2:]))
+	else:
+		print "Usage: python %s [-f<numbers>] areafile" % (argv[0])
+		print "Examples: python %s areas.txt" % (argv[0])
+		print "          python %s -f345 areas.txt" % (argv[0])
+		exit()
+
+	areas = getAreas(filename)
+	printList(filters, areas)
---
+++ pickareas.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+# Program for choosing areas on the lunar surface to use in
+# selecting SMART-1/AMIE images with listImages.py
+
+# The script requires the following image in the same directory:
+# http://commons.wikimedia.org/wiki/File:Moonmap_from_clementine_data.png
+
+from matplotlib import pyplot as plot
+from math import floor
+
+global start_x, start_y, hold, areas
+hold = False
+areas = []
+
+def AMIEcoord(x, y):
+	"""Transform image coordinates to AMIE coordinates.
+	This means longitude ranges between [0, 360] instead of [-180, 180]."""
+	lat = y
+	lon = (x + 360) % 360
+	return (lon, lat)
+
+def click(event):
+	"""On mouse click either start or end area selection.
+	
+	If global variable 'hold' is False, start a new area, mark it with a point.
+	If 'hold' is True, end area, save the area to global list 'areas',
+	and draw the selection box on the map. """
+	
+	global start_x, start_y, hold, areas
+	if event.inaxes:
+		x = event.xdata
+		y = event.ydata
+		if hold:
+			# End selection, save coordinates and draw
+			plot.title("Click for new starting point or close window to write file.")
+			print "End area at (%.2f, %.2f)." % (x, y)
+			hold = False
+			plot.plot([start_x, x, x, start_x, start_x], [start_y, start_y, y, y, start_y], c="red")
+			plot.plot(x,y, ".", c="red")
+			x1, y1 = AMIEcoord(start_x, start_y)
+			x2, y2 = AMIEcoord(x, y)
+			x1, x2 = sorted([x1, x2])
+			y1, y2 = sorted([y1, y2])
+			areas.append((x1, x2, y1, y2))
+		else:
+			# Start making a selection
+			print "Start area at (%.2f, %.2f)." % (x, y)
+			plot.title("Click for new end point.")
+			plot.plot(x,y, ".", c="red")
+			hold = True
+			start_x = x
+			start_y = y
+		
+if __name__=="__main__":
+	# First handle command line arguments:
+	from sys import argv, exit
+	if len(argv) == 2:
+		# For one argument, use it as output filename, overwriting
+		outfile = argv[1]
+		mode = 'w'
+	elif len(argv) == 3 and argv[1] == "-a":
+		# For two arguments, if the first is '-a', use second as
+		# filename and append.
+		outfile = argv[2]
+		mode = "a"
+	else:
+		# With bad command line, print usage message and exit.
+		print "Usage: %s [-a] outputfile" % (argv[0])
+		exit()
+	
+	# Set up plot window and connect 'click' function to mouse click event.
+	moonmap = plot.imread("Moonmap_from_clementine_data.png")
+	plot.connect("button_press_event", click)
+	plot.title("Click for new area starting point. Note: do not make areas that span across the red line.")
+	plot.subplots_adjust(left=0.04, right=0.98, bottom=0.04, top=0.98)
+	plot.axis([-180, 180, -90, 90])
+	plot.grid()
+	plot.yticks(range(-90, 90, 10))
+	plot.xticks(range(-180, 180, 10), range(180,359,10)+range(0,180,10), rotation="90")
+	plot.imshow(moonmap, interpolation=None, extent=[-180, 180, -90, 90])
+	plot.plot([0, 0], [-90, 90], c='red')
+	plot.show()
+	# Program waits at this point until plot window is closed.
+	# Then write areas:
+	if areas:
+		action = {"a":"Added", "w":"Wrote"}
+		try:
+			f = open(outfile, mode)
+		except IOError:
+			print "Error writing file %s!" % (outfile)
+			exit()
+		for area in areas:
+			f.write("%.2f\t%.2f\t%.2f\t%.2f\n" % area)
+		print "%s %d areas to %s." % (action[mode], len(areas), outfile)
+	else:
+		print "No areas were selected. Output file not changed."
